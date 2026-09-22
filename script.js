3df1fba78b44fab51afb77fb9b4c4b0c326aff14
TJ-Manufaktur <tizian3dprintdesign@gmail.com>
2026-09-22 22:38:42 +0200

    Simplify core script loader

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 (()=>{
-  const loadCore=()=>{
-    if(document.querySelector('script[data-tj-core]'))return;
-    const el=document.createElement('script');
-    el.src='script-core.js';
-    el.defer=true;
-    el.dataset.tjCore='1';
-    document.head.appendChild(el);
-  };
-  loadCore();
+  if(document.querySelector('script[data-tj-core]'))return;
+  const el=document.createElement('script');
+  el.src='script-core.js';
+  el.defer=true;
+  el.dataset.tjCore='1';
+  document.head.appendChild(el);
 })();
